32c12afdf9a59f403810098689d4908eef0fad4b
伐宿 <fasu@uni-ubi.com>
2023-10-11 14:30:01 +0800

    feat: update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,3 @@
-// 文字按特定长度分割, 返回一个数组, 换行符也算一个字符
-export const chunkText = (text: string, length: number) => {
-  if (!text) return [];
-  if (!length) return [text];
-
-  const result = [];
-  let i = 0;
-  while (i < text.length) {
-    result.push(text.slice(i, i + length));
-    i += length;
-  }
-  return result;
-};
 
 // 随机生成颜色, 返回一个16进制颜色值
 export const getRandomColor = () => {
--- src/utils/requestEventStream.ts
+++ src/utils/requestEventStream.ts
@@ -1,35 +1,6 @@
 import { message } from 'antd';
 import { MESSAGE_UID } from './constant';
-import { chunkText } from '.';
 import { fetchEventSource } from '@microsoft/fetch-event-source';
-
-const decoder = new TextDecoder('utf-8');
-
-const processResult = async ({ res, callback }: { res?: any; callback: any }) => {
-  const reader = res.body?.getReader();
-  if (!reader) {
-    console.error('没有reader');
-    return;
-  }
-
-  let done = false;
-  while (!done) {
-    const result = await reader.read();
-    if (result.done) {
-      done = true;
-      callback(-1);
-    } else {
-      const decodedData = decoder.decode(result.value, { stream: true });
-      const str = decodedData.toString();
-
-      const chunk = chunkText(str, 1);
-      for (let i = 0; i < chunk.length; i++) {
-        callback(chunk[i]);
-        await new Promise((resolve) => requestAnimationFrame(resolve));
-      }
-    }
-  }
-};
 
 interface IOptions {
   url: string;
@@ -79,31 +50,6 @@ const requestEventStream = async (
       throw new Error('请求出错');
     },
   });
-  return;
-  const res = await fetch(
-    new Request(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-        ...headers,
-      },
-      body: JSON.stringify(data),
-    }),
-    {
-      signal: abortController?.signal,
-    },
-  );
-  if (res.status == 200) {
-    const messageuid = res.headers.get(MESSAGE_UID);
-    if (messageuid) {
-      localStorage.setItem(MESSAGE_UID, messageuid);
-    }
-
-    processResult({ res, callback });
-  } else {
-    onError?.(res);
-    message.error('请求出错');
-  }
 };
 
 export default requestEventStream;
