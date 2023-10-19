579e38a67f4da339bbdf7b3182abe5800f58925e
伐宿 <fasu@uni-ubi.com>
2023-10-19 15:04:24 +0800

    feat: 生成二维码

--- src/pages/AllHistory/index.tsx
+++ src/pages/AllHistory/index.tsx
@@ -1,7 +1,21 @@
+import { totp, generateKey, getKeyUri } from 'otp-io';
+import { hmac, randomBytes } from "otp-io/crypto";
 import useService from './useService';
 import List from './components/List';
 import Content from './components/Content';
 import styles from './index.module.less';
+
+const key = generateKey(randomBytes, /* bytes: */ 20);
+
+const url = getKeyUri({
+  type: "totp",
+  secret: key,
+  name: "faruxue",
+  issuer: "gpt"
+});
+
+// todo: 接入生成二维码
+console.log(url);
 
 const AllHistory = () => {
   useService();
